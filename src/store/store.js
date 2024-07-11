00103b1728ab4624f3b99e1a7df2c46f87d32385
Ankit Jha <ankijha096@gmail.com>
2024-07-11 12:13:54 +0530

    first commit

---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+import {configureStore} from '@reduxjs/toolkit';
+import authSlice from './authSlice';
+
+const store = configureStore({
+  reducer : {
+    auth : authSlice
+    // post : postSlice,
+    // TODO : add more slice here for post
+    
+  }
+})
+
+
+export default store
